9852af6505a4b95e2bbd5205a516d0d6d0f77fbd
JeremTech <jeremtech@gmx.fr>
2020-08-05 21:38:30 +0200

    Updated pre-release number

--- Forge Modding Helper 3/Utils/AppInfos.cs
+++ Forge Modding Helper 3/Utils/AppInfos.cs
@@ -14,7 +14,7 @@ namespace Forge_Modding_Helper_3.Utils
         private static bool isStable = false;
 
         // If "isStable" is set to false, then the current version is the pre-release with the number "pre_release_number"
-        private static int pre_release_number = 3;
+        private static int pre_release_number = 4;
 
         // List of Minecraft versions supported by Forge Modding Helper
         private static List<String> supportedMcVersions = new List<string>() {"1.13.2", "1.14.2", "1.14.3", "1.14.4", "1.15.2", "1.16.1"};
